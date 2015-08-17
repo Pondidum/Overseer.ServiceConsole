1ca2dffbc6755ed0192e8d006ef689b9c2c1a56e
andy <andy@andydote.co.uk>
2015-08-17 20:11:59 +0100

    initial version

---
+++ Overseer.Service/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overseer.Service
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main()
+		{
+			var service = new Service();
+
+			if (Debugger.IsAttached)
+			{
+				service.StartConsole();
+			}
+			else
+			{
+				ServiceBase.Run(new ServiceBase[] { service });
+			}
+		}
+	}
+}
---
+++ Overseer.Service/Service.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+
+namespace Overseer.Service
+{
+	public partial class Service : ServiceBase
+	{
+		public Service()
+		{
+			InitializeComponent();
+		}
+
+		public void StartConsole()
+		{
+			OnStart(new string[] { });
+		}
+
+		protected override void OnStart(string[] args)
+		{
+		}
+
+		protected override void OnStop()
+		{
+		}
+	}
+}
